b8e475c2e211b92a732084b089bd022bce195b10
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-31 23:06:15 -0300

    first commit

---
+++ src/test/java/br/com/rictodolist/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rictodolist.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
