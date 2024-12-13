9981801d0ce1ccda949ea1275e02bbd69c7ce156
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-12-13 13:43:54 -0300

    test: services

---
+++ src/test/java/br/com/rictodolist/todolist/services/AuthServiceIntegrationTest.java
@@ -0,0 +1,63 @@
+package br.com.rictodolist.todolist.services;
+
+import br.com.rictodolist.todolist.dtos.user.UserRequestDTO;
+import br.com.rictodolist.todolist.exceptions.UserNotFoundException;
+import br.com.rictodolist.todolist.infrastructure.security.SecurityConfig;
+import br.com.rictodolist.todolist.repositories.IUserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthServiceIntegrationTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @MockBean
+    private IUserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("Should throw UserNotFoundException when user is not found")
+    public void whenUserNotFound_thenThrowUserNotFoundException() {
+        UserRequestDTO userRequestDto = new UserRequestDTO(
+                "nonExistentUser",
+                null,
+                "rawPassword"
+        );
+
+        when(this.userRepository.findByUsername(userRequestDto.username()))
+                .thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(
+                InternalAuthenticationServiceException.class,
+                () -> {
+                    authService.login(userRequestDto);
+                });
+
+        assertInstanceOf(UserNotFoundException.class, exception.getCause());
+        assertEquals(
+                "Usuário não encontrado: " + userRequestDto.username(),
+                exception.getCause().getMessage());
+
+        verify(userRepository, times(1))
+                .findByUsername(anyString());
+    }
+}
