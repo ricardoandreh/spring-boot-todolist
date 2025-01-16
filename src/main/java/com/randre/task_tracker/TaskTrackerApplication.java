8a6d2ab01880a3107218b33eedc2959c96e0276d
ricardoandreh <ricardo.andre.ifc@gmail.com>
2025-01-16 03:25:26 -0300

    refactor: change IA implementation to Groq Cloud

--- src/main/java/com/randre/task_tracker/TaskTrackerApplication.java
+++ src/main/java/com/randre/task_tracker/TaskTrackerApplication.java
@@ -1,13 +1,30 @@
 package com.randre.task_tracker;
 
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerApplication.class, args);
+    }
 
+    private final OpenAiChatModel chatModel;
+
+    @Autowired
+    public TaskTrackerApplication(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            System.out.println(this.chatModel.call("Generate the names of 5 famous pirates."));
+        };
+    }
 }
